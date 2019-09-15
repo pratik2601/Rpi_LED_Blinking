8e90d3dcab8e5711885841c5fcf8ba2293481de8
pratik2601 <pratik.kadam2601@gmail.com>
2019-09-15 11:01:26 -0700

    Python files added GPIO18 and GPIO23

---
+++ GPIO18_Blinking.py
@@ -0,0 +1,24 @@
+#simple blinking at GPIO 18
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+# Pin Definitions
+ledPin = 18
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin,GPIO.OUT)
+#initliaze it to LOW
+GPIO.output(ledPin, GPIO.LOW)
+
+# Infinite Loop
+#try:
+while 1:
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.5)
+
+#except KeyboardInterrupt:
+#    GPIO.cleanup()
