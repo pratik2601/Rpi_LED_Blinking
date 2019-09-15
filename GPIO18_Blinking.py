a0025417850317ec6b283c5e8b120d27915a17bf
pratik2601 <pratik.kadam2601@gmail.com>
2019-09-16 02:20:10 +0530

    Revert "Revert "LED blinking speed changed""
    This reverts commit cf0da79b0b66add8efaa12ca081da8ba4fb12d03.

--- GPIO18_Blinking.py
+++ GPIO18_Blinking.py
@@ -16,9 +16,9 @@ GPIO.output(ledPin, GPIO.LOW)
 #try:
 while 1:
     GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(0.5)
+    time.sleep(0.25)
     GPIO.output(ledPin, GPIO.LOW)
-    time.sleep(0.5)
+    time.sleep(0.25)
 
 #except KeyboardInterrupt:
 #    GPIO.cleanup()
