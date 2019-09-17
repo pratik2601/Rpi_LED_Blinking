c009aafff4b35ab632cab9eb0c7ec4314a8ed02e
pratik2601 <pratik.kadam2601@gmail.com>
2019-09-17 17:57:22 +0530

    GPIO24 added to system

---
+++ GPIO24_Blinking.py
@@ -0,0 +1,25 @@
+#simple blinking at GPIO 23
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+# Pin Definitions
+ledPin = 24
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin,GPIO.OUT)
+#initliaze it to LOW
+GPIO.output(ledPin, GPIO.LOW)
+
+# Infinite Loop
+#try:
+while 1:
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.1)
+
+#except KeyboardInterrupt:
+#    GPIO.cleanup()
+
