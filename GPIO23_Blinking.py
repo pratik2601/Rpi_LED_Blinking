25e324beee55f6d3813d49d2d0a87898fc7614f4
pratik2601 <pratik.kadam2601@gmail.com>
2019-09-17 17:45:20 +0530

    Revert "GPIO23 blinking rate reduced"
    This reverts commit f6a43a16009b60a68ee357d06d962e5ed4d7575f.

--- GPIO23_Blinking.py
+++ GPIO23_Blinking.py
@@ -16,9 +16,9 @@ GPIO.output(ledPin, GPIO.LOW)
 #try:
 while 1:
     GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(2)
+    time.sleep(0.1)
     GPIO.output(ledPin, GPIO.LOW)
-    time.sleep(2)
+    time.sleep(0.1)
 
 #except KeyboardInterrupt:
 #    GPIO.cleanup()
